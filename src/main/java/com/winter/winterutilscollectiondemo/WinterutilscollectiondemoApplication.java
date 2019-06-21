667eb22993d1a2b92623a6afaece3fe4f6d311ad
Winteer <isnowash@yeah.net>
2019-06-21 10:25:43 +0800

    build project.

---
+++ src/main/java/com/winter/winterutilscollectiondemo/WinterutilscollectiondemoApplication.java
@@ -0,0 +1,13 @@
+package com.winter.winterutilscollectiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterutilscollectiondemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinterutilscollectiondemoApplication.class, args);
+    }
+
+}
